1985c36b0a43405cfde38ac63e8242d232f037f0
Oren Panitch <Oren_Panitch@comcast.com>
2018-10-20 23:27:16 -0400

    set up JS functionality.  works to select random places and shows all of selected list.  default KOP

--- includes/js/core.js
+++ includes/js/core.js
@@ -1,54 +1,84 @@
 (function(doc, $) {
   var $doc = $(doc),
+    activeList = '',
     classes = {
-      active: 'wte-place--choice'
+      active: 'wte-place--choice',
+      showAll: 'wte-place-list--show'
     };
 
-  $.jQueryRandom = 0;
-  $.extend($.expr.pseudos, {
-    random: function(a, i, m, r) {
-      console.log('extend');
-      console.log('a', a);
-      console.log('i', i);
-      console.log('m', m);
-      console.log('r', r);
-      if (i === 0) {
-        $.jQueryRandom = Math.floor(Math.random());
-      }
-
-      console.log('i after', i);
-
-      return $.jQueryRandom === i;
-    }
-  });
-
-  function refresh(e) {
-    console.log('refresh', e);
-    console.log('refresh keycode', e.keyCode);
-    if (!e.hasOwnProperty('keyCode') || e.keyCode === 32) {
-      $('dt').removeClass(classes.active);
-      $('dt:random').addClass(classes.active);
-    }
+  /**
+   * Returns a random integer between min (inclusive) and max (inclusive)
+   * Using Math.round() will give you a non-uniform distribution!
+   */
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function showAll() {
-    $('dl').toggleClass('show-all');
+  function setActiveList() {
+    var hash = window.location.hash ? window.location.hash : 'kop';
+
+    activeList = hash;
+  }
+
+  function getChoice() {
+    var $currentList = $('.wte-place-list').filter('#' + activeList),
+      $listItems = $currentList.find('li'),
+      randomNum = getRandomInt(0, $listItems.length);
+
+    return $listItems.eq(randomNum);
+  }
+
+  function setChoice($choice) {
+    var content = $choice.html(),
+      $displayChoice = $('#displayChoice');
+
+    $displayChoice.html(content);
+  }
+
+  function hideAll() {
+    $('.wte-place-list').removeClass(classes.showAll);
+  }
+
+  function showAll(listId) {
+    $('.wte-place-list')
+      .filter('#' + listId)
+      .addClass(classes.showAll);
+  }
+
+  function handleUserAction(event) {
+    var keyPress =
+        event.hasOwnProperty('keyCode') && event.keyCode === 32 ? true : false,
+      btnPress = !event.hasOwnProperty('keyCode'),
+      $nextChoice,
+      isListVisible =
+        $('.wte-place-list').filter('.' + classes.showAll).length > 0;
+
+    if (isListVisible) {
+      hideAll();
+    }
+
+    if (keyPress || btnPress) {
+      $nextChoice = getChoice();
+      setChoice($nextChoice);
+    }
   }
 
   function applyBindings() {
     // Refresh on Next Button click
-    $doc.on('click', '#next', refresh);
+    $doc.on('click', '#next', handleUserAction);
     // Refresh on key press
-    $doc.on('keydown', refresh);
+    $doc.on('keydown', handleUserAction);
     // Show All Button click
-    $doc.on('click', '#all', showAll);
+    $doc.on('click', '#show-all', function() {
+      showAll(activeList);
+    });
   }
 
   function init() {
     // if restaurant list exists
-    console.log('starting randomizer');
     applyBindings();
-    $('#next').click();
+    setActiveList();
+    setChoice(getChoice());
   }
 
   init();
